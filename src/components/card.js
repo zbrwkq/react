7bbdcabcb976bd4ec68e43b5eb9a0cf746aaeeb7
zbrwkq <Damien.chv78@gmail.com>
2023-05-22 17:44:40 +0200

    second commit

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,11 @@
 const Card = ({ title, content, img }) => {
     return (
         <div className='card'>
-            {img !== undefined ? <img src={img} alt={title} /> : ''}
-            <h3>{title}</h3>
-            <p>{content}</p>
+            {img !== undefined ? <img src={img} alt={title}  className="card-img-top"/> : ''}
+            <div className="card-body">
+                <h3 className="card-title">{title}</h3>
+                <p className="card-text">{content}</p>
+            </div>
         </div>
     )
 }
